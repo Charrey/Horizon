66ab46346b7163c29aad79be496a82812b9e04fb
Pim van Leeuwen <p.vanleeuwen@student.utwente.nl>
2017-10-20 17:45:25 +0200

    Work by Adrian part 2

--- config/routes.rb
+++ config/routes.rb
@@ -1,23 +1,35 @@
 Rails.application.routes.draw do
   devise_for :users, :controllers => { registrations: 'registrations' }
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  #users controller
   get 'signup' => 'users#new'
   get 'users' => 'users#new'
+
+  #roleplays controller
+  delete '/roleplays/:roleplay_id' => 'roleplays#destroy'
+  post '/roleplays/start/:id' => 'roleplays#start'
+  post '/roleplays/stop/:id' => 'roleplays#stop'
+  post '/roleplays/add_dummy/:id' => 'roleplays#add_dummy'
+  get '/roleplay/new' => 'roleplays#new'
+  get '/roleplays/message_to_append/:character_id', to: 'roleplays#message_to_append', as: 'message_to_append'
+  resources :roleplays
+ 
+  #messages controller
+  post 'messages/save_message', to: 'messages#save_message', as: 'save_message'
+ 
+
   post '/' => 'sessions#create'
   delete '/' => 'sessions#destroy'
   get '/dashboard' => 'dashboard#index'
   root 'dashboard#index'
-  get '/roleplay/new' => 'roleplays#new'
-  delete '/roleplays/:roleplay_id' => 'roleplays#destroy'
+  
+  
   post  '/characters/claim' => 'characters#claim'
   delete '/characters/:character' => 'characters#destroy'
   post  '/characters/unbind' => 'characters#unbind'
   post '/characters/assign' => 'characters#assign'
-  post '/roleplays/start/:id' => 'roleplays#start'
-  post '/roleplays/stop/:id' => 'roleplays#stop'
-  post '/roleplays/add_dummy/:id' => 'roleplays#add_dummy'
+ 
   resources :users
-  resources :roleplays
+  
   resources :characters
   mount ActionCable.server => '/cable'
 end
